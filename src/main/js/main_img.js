b739d2f077fd936699445c555a65ef197be786e9
LeeTaeYoung <xodud5080@naver.com>
2024-11-07 20:55:08 +0900

    main_img 구현 완료

--- src/main/js/main_img.js
+++ src/main/js/main_img.js
@@ -11,24 +11,23 @@ const Main_img = () => {
     useEffect(() => {
         const timer = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000);
+        }, 2000);
 
         return () => clearInterval(timer); // 컴포넌트가 언마운트될 때 타이머 정리
     }, [images.length]);
 
     return(
         <div className="main_img_container">
-            <div style={{position:'absolute', width:"100%"}}>
-                <div style={{position:"relative", width:"1400px", margin:"auto"}}>
-                    <a style={{position:"absolute", top:"210px", display:"inline-block", height:"20px"}}>1</a>
-                    <a style={{position:"absolute", top:"210px", right:"0", display:"inline-block", height:"20px"}}>2</a>
+            <div className='main_img_slide_container'>
+                <div className='main_img_slide_content'>
+                    <svg className='main_img_slide_prev' onClick={() => setCurrentIndex((currentIndex - 1 + images.length) % images.length)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15 18l-6-6 6-6" stroke="currentColor" strokeWidth="0.7" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                    <svg className='main_img_slide_next' onClick={() => setCurrentIndex((currentIndex + 1) % images.length)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9 18l6-6-6-6" stroke="currentColor" strokeWidth="0.7" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
                 </div>
             </div>
-            {/* <div className='main_img_prev_content'>
-                <svg className='main_img_prev' onClick={() => setCurrentIndex((currentIndex - 1 + images.length) % images.length)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15 18l-6-6 6-6" stroke="currentColor" strokeWidth="0.7" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-            </div> */}
             <div className='main_img_content'>
                 {images.map((image, index) => (
                     <img
@@ -38,11 +37,6 @@ const Main_img = () => {
                     />
                 ))}
             </div>
-            {/* <div className='main_img_next_content'>
-                <svg className='main_img_next' onClick={() => setCurrentIndex((currentIndex + 1) % images.length)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 18l6-6-6-6" stroke="currentColor" strokeWidth="0.7" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-            </div> */}
         </div>
     )
 }
