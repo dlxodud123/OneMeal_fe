c160d279f387f2a205542bb1df42ba551c435316
LeeTaeYoung <xodud5080@naver.com>
2024-11-14 15:54:22 +0900

    signup페이지 ui 구현 완료 및 javascript문 구현중

--- src/signup/js/signup_form.js
+++ src/signup/js/signup_form.js
@@ -1,8 +1,59 @@
 import './../css/signup_form.css';
 import Header from '../../common/header/js/header';
 import Footer from '../../common/footer/js/footer';
+import { MyContext } from '../../App';
+import { useContext, useRef } from 'react';
 
 const Signup_form = () => {
+    const {api} = useContext(MyContext);
+
+    // 우편번호api
+    const zonecodeRef = useRef(null);
+    const addressRef = useRef(null);
+
+    const openDaumPostcode = () => {
+        new window.daum.Postcode({
+            oncomplete: function (data) {
+                const { zonecode, address, bname, buildingName } = data;
+                
+                if (zonecodeRef.current) zonecodeRef.current.value = zonecode;
+                const formattedAddress = `${address} ${bname ? `(${bname}` : ''}${bname && buildingName ? ', ' : ''}${buildingName ? buildingName : ''}${bname || buildingName ? ')' : ''}`;
+                
+                if (addressRef.current) {
+                    addressRef.current.value = formattedAddress;
+                }
+            },
+        }).open();
+    };
+    // 생년월일 반복문
+    const years = [];
+    for (let year = 2024; year >= 1941; year--) {
+        years.push(year);
+    }
+    const months = [];
+    for (let month = 12; month >= 1; month--) {
+        months.push(month);
+    }
+    const days = [];
+    for (let day = 31; day >= 1; day--) {
+        days.push(day);
+    }
+
+    // 휴대전화 입력 focus
+    const phoneSecondRef = useRef(null);
+    const handlePhoneSecondChange = (e) => {
+        if (phoneSecondRef.current) {
+            phoneSecondRef.current.focus();
+        }
+    }
+    const phoneThirdRef = useRef(null);
+    const handlePhoneThirdChange = (e) => {
+        if (e.target.value.length === 4) {
+            if (phoneThirdRef.current) {
+                phoneThirdRef.current.focus();
+            }
+        }
+    }
 
     return(
         <div className='signup_form_container'>
@@ -35,7 +86,7 @@ const Signup_form = () => {
                                         <label className='signup_form_default_info_id_input_copy'>중복확인</label>
                                     </div>
                                     <div className='signup_form_default_info_id_input_explain_content'>
-                                        <label className='signup_form_default_info_id_input_explain'>공백없는 6~12자의 문자를 입력해야 합니다.</label>
+                                        <label className='signup_form_default_info_id_input_explain'>6~12자의 문자를 입력해야 합니다.</label>
                                     </div>
                                 </td>
                             </tr>
@@ -50,10 +101,10 @@ const Signup_form = () => {
                                 </th>
                                 <td className='signup_form_default_info_pwd_input_container'> 
                                     <div className='signup_form_default_info_pwd_input_content'>
-                                        <input className='signup_form_default_info_pwd_input' type='text'></input>
+                                        <input className='signup_form_default_info_pwd_input' type='password' maxLength={16}></input>
                                     </div>
                                     <div className='signup_form_default_info_pwd_input_explain_content'>
-                                        <label className='signup_form_default_info_pwd_input_explain'>공백없는 8~14자의 문자를 입력해야 합니다.</label>
+                                        <label className='signup_form_default_info_pwd_input_explain'>8~16자의 문자를 입력해야 합니다.</label>
                                     </div>
                                 </td>
                             </tr>
@@ -68,7 +119,7 @@ const Signup_form = () => {
                                 </th>
                                 <td className='signup_form_default_info_pwd_check_input_container'> 
                                     <div className='signup_form_default_info_pwd_check_input_content'>
-                                        <input className='signup_form_default_info_pwd_check_input' type='text'></input>
+                                        <input className='signup_form_default_info_pwd_check_input' type='password' maxLength={16}></input>
                                     </div>
                                 </td>
                             </tr>
@@ -100,8 +151,8 @@ const Signup_form = () => {
                                     </div>
                                 </th>
                                 <td className='signup_form_default_info_phone_input_container'> 
-                                    <select className='signup_form_default_info_phone_first_input_content'>
-                                        <option value>선택</option>
+                                    <select onChange={handlePhoneSecondChange} className='signup_form_default_info_phone_first_input_content'>
+                                        <option value="">선택</option>
                                         <option value="010">010</option>
                                         <option value="011">011</option>
                                         <option value="016">016</option>
@@ -110,11 +161,11 @@ const Signup_form = () => {
                                     <div className='signup_form_default_info_phone_input_blank_content'>
                                         <label className='signup_form_default_info_phone_input_blank'>-</label>
                                     </div>
-                                    <input className='signup_form_default_info_phone_second_input' maxLength={4} type='text' ></input>
+                                    <input onChange={handlePhoneThirdChange} ref={phoneSecondRef} className='signup_form_default_info_phone_second_input' maxLength={4} type='text' ></input>
                                     <div className='signup_form_default_info_phone_input_blank_content'>
                                         <label className='signup_form_default_info_phone_input_blank'>-</label>
                                     </div>
-                                    <input className='signup_form_default_info_phone_third_input' maxLength={4} type='text' ></input>
+                                    <input ref={phoneThirdRef} className='signup_form_default_info_phone_third_input' maxLength={4} type='text' ></input>
                                 </td>
                             </tr>
                         </tbody>
@@ -143,8 +194,98 @@ const Signup_form = () => {
                                 주소
                             </div>
                             <div className='signup_form_select_info_address_content'>
-                                asdf
+                                <div className='signup_form_select_info_address_btn_content'>
+                                    <input readOnly ref={zonecodeRef} className='signup_form_select_info_address_zonecode'>
+                                    </input>
+                                    <div onClick={openDaumPostcode} className='singup_form_select_info_address_btn'>
+                                        우편번호
+                                    </div>
+                                </div>
+                                <input readOnly ref={addressRef} className='signup_form_select_info_address'>
+                                </input>
+                                <input className='signup_form_select_info_detail_address'></input>
                             </div>
+                        </div>
+
+                        <div className='signup_form_select_info_birth_container'>
+                            <div className='signup_form_select_info_birth_subject_content'>
+                                생년월일
+                            </div>
+                            <div className='signup_form_select_info_birth_content'>
+                                <div style={{display:"flex"}}>
+                                    <select className='signup_form_select_info_birth'>
+                                        <option value=""></option>
+                                        {years.map((year) => (
+                                            <option key={year} value={year}>
+                                                {year}
+                                            </option>
+                                        ))}
+                                    </select>
+                                    <div className='signup_form_select_info_birth_serve'>
+                                        년
+                                    </div>
+                                </div>
+                                <div style={{display:"flex"}}>
+                                    <select className='signup_form_select_info_birth'>
+                                        <option value=""></option>
+                                        {[...months].reverse().map((month) => (
+                                            <option key={month} value={month}>
+                                                {month}
+                                            </option>
+                                        ))}
+                                    </select>
+                                    <div className='signup_form_select_info_birth_serve'>
+                                        월
+                                    </div>
+                                </div>
+                                <div style={{display:"flex"}}>
+                                    <select className='signup_form_select_info_birth'>
+                                        <option value=""></option>
+                                        {[...days].reverse().map((day) => (
+                                            <option key={day} value={day}>
+                                                {day}
+                                            </option>
+                                        ))}
+                                    </select>
+                                    <div className='signup_form_select_info_birth_serve'>
+                                        일
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='signup_form_select_info_gender_container'>
+                            <div className='signup_form_select_info_gender_subject_content'>
+                                성별
+                            </div>   
+                            <div className='signup_form_select_info_gender_content'>
+                                <div style={{display:"flex"}}>
+                                    <div className='signup_form_select_info_gender_choice_content'>
+                                        <input type='radio' name='gender' value="female" checked />
+                                    </div>
+                                    <div className='signup_form_select_info_gender_choice_serve'>
+                                        여성
+                                    </div>
+                                </div>
+                                <div style={{display:"flex"}}>
+                                    <div className='signup_form_select_info_gender_choice_content'>
+                                        <input type='radio' name='gender' value="male" />
+                                    </div>
+                                    <div className='signup_form_select_info_gender_choice_serve'>
+                                        남성
+                                    </div>
+                                </div>
+                            </div>             
+                        </div>
+                    </div>
+                    
+                    <div className='signup_form_btn_container'>
+                        <div className='signup_from_back_btn'>
+                            뒤로
+                        </div>
+                        <div className='signup_form_btn_gap'></div>
+                        <div className='signup_form_btn'>
+                            회원가입
                         </div>
                     </div>
                 </div>
